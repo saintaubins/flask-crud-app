a57bb098a62089bad8accc02179403bf4fcc868f
saintaubins <saintaubins@gmail.com>
2020-11-25 12:05:39 -0500

    added authentication

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
  
  
  
  
 app = Flask(__name__)
 app.secret_key = "Secret Key"
- 
+bootstrap = Bootstrap(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login' 
 ENV = 'prod'
 
 if ENV == 'dev':
@@ -19,7 +29,27 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
- 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
  
 #Creating model table for our CRUD database
 class Data(db.Model):
@@ -39,11 +69,58 @@ class Data(db.Model):
 #query on all our employee data.
 @app.route('/')
 def Index():
+    #all_data = Data.query.all()
+ 
+    return render_template("index.html")#, employees = all_data)
+
+@app.route('/crud')
+@login_required
+def crud():
     all_data = Data.query.all()
  
-    return render_template("index.html", employees = all_data)
+    return render_template("crud.html", employees = all_data)
  
- 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('crud'))
+
+        return '<h1>Invalid username or password</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('Index'))
  
 #this route is for inserting data to mysql database via html forms
 @app.route('/insert', methods = ['POST'])
@@ -62,7 +139,7 @@ def insert():
  
         flash("Employee Inserted Successfully")
  
-        return redirect(url_for('Index'))
+        return redirect(url_for('crud'))
  
  
 #this is our update route where we are going to update our employee
@@ -79,7 +156,7 @@ def update():
         db.session.commit()
         flash("Employee Updated Successfully")
  
-        return redirect(url_for('Index'))
+        return redirect(url_for('crud'))
  
  
  
@@ -92,7 +169,7 @@ def delete(id):
     db.session.commit()
     flash("Employee Deleted Successfully")
  
-    return redirect(url_for('Index'))
+    return redirect(url_for('crud'))
  
  
  #Driver Code
